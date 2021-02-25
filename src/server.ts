289711766c4a6c6e7f85d001b2a2ddcafe2114ef
André Macena <andrmacena_@outlook.com>
2021-02-24 23:30:55 -0300

    refactor: Removido todo codigo referente a inicializacao do app

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,5 @@
-import 'reflect-metadata'
-import express from 'express'
-import './database'
-import { router } from './routes'
-
-const app = express()
+import { app } from "./app"
 
 const PORT = 3333
-
-app.use(express.json())
-app.use(router)
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
